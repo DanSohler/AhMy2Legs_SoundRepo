7c0b637ef790d468823e1119312cd42f5e406629
Daniel Sohler <daniel.sohler73@gmail.com>
2021-07-19 12:11:11 +1000

    Added Audio Manager Script & Sounds Folder,
    Added 2 scripts (Sounds template script, and an AudioManager script)
    Added Sounds folder, with sub-folders for the individuals working on this project. (Dan, Josh, Ryan)

---
+++ AhMy2Legs/Assets/Scripts/AudioManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManagerScript : MonoBehaviour
+{
+    // creates array for inidivual sounds
+    public Sounds[] sounds;
+
+    void Awake ()
+    {
+        // loop that checks all current sounds, and gives them the volume and pitch variables
+        foreach (Sounds s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+
+        }
+    }
+    // function that will be called in other scripts to call upon sounds to be played
+    public void Play(string name)
+    {
+        Sounds s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
